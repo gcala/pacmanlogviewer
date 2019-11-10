22618bd0e1435ee614b9f03e79462adcb6a84dd2
Giuseppe Calà <jiveaxe@gmail.com>
2019-11-10 14:19:14 +0100

    updated copyright info

--- defines.h
+++ defines.h
@@ -1,6 +1,6 @@
 /*
     Pacman Log Viewer
-    Copyright (C) 2012-2015 Giuseppe Calà <jiveaxe@gmail.com>
+    Copyright (C) 2012-2019 Giuseppe Calà <jiveaxe@gmail.com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
